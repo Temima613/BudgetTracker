1d04ee6905dad4a8e1a349e862136fb900cba7c7
Chana Leah Nissel <clnissel@wits.edu>
2025-03-11 22:43:30 -0400

    calculating balance functions

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 //Program starts when the DOM is loaded
 document.addEventListener('DOMContentLoaded', function() 
 {
-}
+    let balance=0;
+    
+    //Displays balance on page
+    function displayBalance()
+    {
+        document.getElementById('balanceDisplay').textContent=balance;
+    }
+
+    //Handles income form
+    document.getElementById('incomeAmount').addEventListener('click', function()
+    {
+        //Adds the most recently inputted income value to balance
+        let incomeAmount=document.getElementById('incomeAmount').value;
+        balance=balance+incomeAmount;
+
+        //Display updated balance
+        displayBalance();
+    });
+
+    //Handles expense form
+    document.getElementById('expenseAmount').addEventListener('click', function()
+    {
+        //Decreases balance by the most recently inputted income value
+        let expenseAmount=document.getElementById('expenseAmount').value;
+        balance=balance-expenseAmount;
+
+        //Display updated balance
+        displayBalance();
+    });
+//IN HTML:
+//Change id for input amount to incomeAmount
+//Create paragraph element with id="balanceDisplay" to insert the balance into
+
+});
